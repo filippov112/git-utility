cd08eeb978fc680c36a46a4823ef4ce7c53ee2b7
IlyaFilippov <ilya19xx2014@gmail.com>
2025-05-31 18:40:42 +0300

    Добавил обработку исключительных ситуаций, вынес шаблоны readme.md и gitignore во внешние файлы. Исправил проблему с чтением файла окружения.

--- git-utility/CommitNode.cs
+++ git-utility/CommitNode.cs
@@ -6,10 +6,10 @@ namespace GitUtility
     {
         public Guid Id { get; set; }
         public int Number { get; set; }
-        public string Hash { get; set; }
-        public string Message { get; set; }
+        public string Hash { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; }
         public Guid? ParentId { get; set; }
-        public string BranchName { get; set; }
+        public string BranchName { get; set; } = string.Empty;
     }
 }
